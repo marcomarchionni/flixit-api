595a1febf217b4e72f7da92aac69e8cd07e75fc9
Marco Marchionni <marco.marchionni@gmail.com>
2023-03-30 11:21:35 +0200

    Updated cors policy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const dbConnection = require('./src/db-connection/db-connection');
 const routes = require('./src/routes/routes');
 const {
@@ -9,6 +8,7 @@ const {
   invalidPathHandler,
   errorLogger,
 } = require('./src/error-handling/error-handlers');
+const { corsConfig } = require('./src/middleware/cors-config');
 
 const app = express();
 
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Allow all Cross Origin Requests
-app.use(cors());
+app.use(corsConfig);
 
 // Routes
 app.use(routes);
