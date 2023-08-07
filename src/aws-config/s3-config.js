c95567a485a966d1e0088434dd624b33825c0ca5
Marco Marchionni <marco.marchionni@gmail.com>
2023-08-07 22:34:59 +0200

    Updated S3 config

--- src/aws-config/s3-config.js
+++ src/aws-config/s3-config.js
@@ -2,8 +2,10 @@ const { S3Client } = require('@aws-sdk/client-s3');
 
 exports.s3Client = new S3Client({
   region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT,
-  forcePathStyle: true,
+  endpoint: process.env.AWS_ENDPOINT || undefined,
+  forcePathStyle: process.env.FORCE_PATH_STYLE
+    ? JSON.parse(process.env.FORCE_PATH_STYLE)
+    : undefined,
 });
 
 exports.s3BaseParams = {
